31630368cfabb1f9fceb65836d9647d194953828
Crizer <553784520@qq.com>
2018-02-01 10:32:58 +0800

    add jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
@@ -30,7 +31,6 @@ class AuthorizationsController extends Controller
 
             $oauthUser = $driver->userFromToken($token);
         } catch (\Exception $e) {
-            dd($e);
             return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
 
@@ -58,5 +58,44 @@ class AuthorizationsController extends Controller
         }
 
         return $this->response->array(['token' => $user->id]);
+    }
+
+    /**
+     * @param AuthorizationRequest $request
+     */
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        //filter_var($username, FILTER_VALIDATE_EMAIL) ?
+        //    $credentials['email'] = $username :
+        //    $credentials['phone'] = $username;
+
+        $credentials['phone'] = $request->phone;
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+            'expires_in'   => \Auth::guard('api')->factory()->getTTL() * 60,
+        ])->setStatusCode(201);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return $this->response->noContent();
     }
 }
